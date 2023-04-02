d16c7ddb8fe92632a4ff1b5f399321676b2c430b
h0mely <1577190811@qq.com>
2023-04-02 16:56:08 +0800

    pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,28 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  
+  if(argc > 1){
+    fprintf(2, "Usage: pingpong...\n");
+    exit(1);
+  }
+
+  int p[2];
+  int pid = fork();
+  if (pid > 0) {
+    char buffer[6];
+    read(p[0], buffer, 6);
+    printf("%d: received pong\n", getpid());
+    exit(0);
+  } else if (pid == 0) {
+    printf("%d: received ping\n", getpid());
+    write(p[1], "hello", 6);
+    exit(0);
+  } else {
+    printf("fork error\n");
+  }
+}
